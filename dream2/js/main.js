fb3b5afd9bfcc1f483a629835b688c29ee4247e7
jooni02 <157003315+jooni02@users.noreply.github.com>
2024-02-08 18:01:21 +0900

    .

--- dream2/js/main.js
+++ dream2/js/main.js
@@ -1,28 +1,85 @@
-$(document).ready(function(){
-	console.log('안녕하세요')
-    /* 브라우저를 스크롤했을 때 맨위면 header에 fixed라는 클래스를 삭제
-        조금이라도 아래로 스크롤 된 상타라면 header에 fixed라는 클래스 줌 
-    
-        1. 브라우저가 스크롤 된 정도를 표시한 값
-            $(window).scrollTop() -> 너무길어서 변수로 대신 부르는걸 let sorolling으로 부를수있음
-        2. 만약 스크롤 값이 0이라면
-            header에 fixed라는 클래스를 삭제
-            만약 스크롤값이 0보다 크다면
-            header에 fixed라는 클래스를 줌
-        3. 다 작업 후 console은 주석으로 걸어주기
+$(document).ready(function(){ /* html 로딩 시 단 한번만 실행함 */
+    let scrolling // 스크롤 얼만큼 되었나
 
-    */
-    $(window).scroll(function(){
-        let scrolling = $(window).scrollTop()  
-        //console.log(scrolling)
-        // 조건문 - scrolling값이 0보다 크면 (충족시)
-        if(scrolling > 0){
-            //console.log('0보다크다');
-            $(".header").addClass("fixed");
-        }else{ //0일때 - 맨 위에 있을 때 (충족이 안될시)
-            //console.log('0이다');
-            $(".header").removeClass("fixed");
+    function scroll_chk(){ // 1)반복할 함수를 선언하면 /* 다회용 function*/
+        scrolling = $(window).scrollTop() //let은 한번만
+        console.log(scrolling)
+        if(scrolling > 0){ // 스크롤 된 값이 0보다 크면 - fixed 클래스 추가
+            $('.header').addClass('fixed')
+        }else{ // 같거나 작거나 - fixed 클래스 삭제
+            $('.header').removeClass('fixed')
         }
+    }
+    scroll_chk() // 2)함수의 호출을 함
 
-    });//$(window).scroll
+    $(window).scroll(function(){ //브라우저가 스크롤 될때마다 한번만 실행
+        scroll_chk()
+    }) //$(window).scroll
+
+    /* pc버전시 메뉴에 오버하면 hedaer 스타일 변경 */
+    let window_w // 브라우저 넓이
+    let device_status // 모바일모드인지 pc모드인지 구분
+    let mobile_size = 640
+    function device_chk(){
+        window_w = $(window).width() //브라우저 넓이 저장
+        if(window_w > mobile_size){ //pc버전 
+            devuce_status = 'pc'
+        }else{ //640부터 그 이하
+            device_status = 'mobile'
+        }
+        //console.log(device_status)
+    }//function device_chk
+    device_chk() // 함수호출
+    $(window).resize(function(){
+
+    })
+    $('.header .gnb').on('mouseenter focusin', function(){ /* 일회용 function*/
+        if(device_status == 'pc'){
+            $('.header').addClass('menu_over')
+        }
+    })
+    $('.header').on('mouseleave', function(){ /* 일회용 function*/
+        $('.header').removeClass('menu_over')
+    })
+
+    //메뉴 바로 뒤에는 로그인 버튼이 있음, 로그인 버튼에 포커스 되면 메뉴의 오버상태 해제
+    $('.header .tnb .login').on('focusin',function(){
+        $('.header').removeClass('menu_over')
+    })
+
+    /* 모바일 메뉴 열기/닫기 */
+    $('.header .gnb .gnb_open').on('click', function(){
+        $('.header').addClass('menu_open')
+    })
+    $('.header .gnb .gnb_close').on('click', function(){
+        $('.header').removeClass('menu_open')
+    })
+
+    $(".header .gnb ul.depth1 > li > a").on("click", function(e){
+	/* a 태그의 href를 작동 시키지 않음 */
+        if(device_status == 'mobile'){
+            e.preventDefault();
+            $(this).parent().toggleClass('sub_open')
+        }      
+    });
+
+    /* top버튼 클릭하면 위로 올라가라는 의미*/
+    $('.footer .top').on('click',function(){
+        $('html, body').animate({
+            scrollTop : 0
+        }, 500)
+    })
+
+    // 팝업창 
+    const swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 3000,
+            disableOnInteraction: true,
+        },
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+    });
+
 });//$(document).ready
