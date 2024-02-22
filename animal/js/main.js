4f719e34a66a47bf7986be4e0c910f3b7653097e
jooni02 <157003315+jooni02@users.noreply.github.com>
2024-02-22 18:01:19 +0900

    .

---
+++ animal/js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    /*  */
+
+    /*
+        1차 메뉴에 (.header .depth1 > li)에 마우스를 오버하면  
+        오버한 li (this) 에 sub_over 클래스를 추가
+        header에는 menu_over 클래스 추가
+    */
+   $('.header .gnb ul.depth1 > li').on('mouseenter focusin', function(){
+        $('.header .gnb ul.depth1 > li').removeClass('sub_over')
+        $(this).addClass('sub_over')
+        $('header').addClass('menu_over')
+   })
+   $('.header .gnb').on('mouseleave', function(){
+        $('.header .gnb ul.depth1 > li').removeClass('sub_over')
+        $('header').removeClass('menu_over')
+    })
+    $('.header .tnb .search').on('focusin', function(){
+        $('.header .gnb ul.depth1 > li').removeClass('sub_over')
+        $('header').removeClass('menu_over')
+    })
+
+    /* swiper */
+        const swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+	autoplay: {  /* 팝업 자동 실행 */
+    delay: 3500,
+    disableOnInteraction: true,
+    },
+
+    loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+    navigation: {  /* 이전, 다음 버튼 */
+        nextEl: '.visual .next',  /* 다음 버튼의 클래스명 */
+        prevEl: '.visual .prev',  
+    },
+
+    });
+    swiper.autoplay.stop();  /* 일시정지 기능 */
+    swiper.autoplay.start();  /* 재생 기능 */
+
+})
