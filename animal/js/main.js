4b98f2694863eccf91f8b4296211d0f9c35d4405
jooni02 <157003315+jooni02@users.noreply.github.com>
2024-02-26 16:45:30 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -6,18 +6,18 @@ $(document).ready(function(){
         오버한 li (this) 에 sub_over 클래스를 추가
         header에는 menu_over 클래스 추가
     */
-   $('.header .gnb ul.depth1 > li').on('mouseenter focusin', function(){
+    $('.header .gnb ul.depth1 > li').on('mouseenter focusin', function(){
         $('.header .gnb ul.depth1 > li').removeClass('sub_over')
         $(this).addClass('sub_over')
-        $('header').addClass('menu_over')
-   })
-   $('.header .gnb').on('mouseleave', function(){
+        $('.header').addClass('menu_over')
+    })
+    $('.header .gnb').on('mouseleave', function(){
         $('.header .gnb ul.depth1 > li').removeClass('sub_over')
-        $('header').removeClass('menu_over')
+        $('.header').removeClass('menu_over')
     })
     $('.header .tnb .search').on('focusin', function(){
         $('.header .gnb ul.depth1 > li').removeClass('sub_over')
-        $('header').removeClass('menu_over')
+        $('.header').removeClass('menu_over')
     })
 
     /* swiper */
@@ -102,6 +102,69 @@ $(document).ready(function(){
         $('.adopt .ctrl_btn .stop').show()
     })
 
+    /* 관련사이트
+        site_open을 클릭하면 목록이 나타남
+            site_list 목록이 나타남
+            site_open 사라짐
+            site_close 나타남
+        site_close을 클릭하면 목록이 사라짐 
+            site_list 목록이 사라짐
+            site_open 사라짐
+            site_close 나타남
+    */
 
+    $('.footer .family_site .site_open').on('click', function(){
+        $('.footer .family_site .site_list').slideDown()
+        $(this).hide()
+        $('.footer .family_site .site_close').show()
+    })
+    $('.footer .family_site .site_close').on('click', function(){
+        $('.footer .family_site .site_list').slideUp()
+        $(this).hide()
+        $('.footer .family_site .site_open').show()
+    })
+
+
+    /*
+        메뉴열기를 클릭하면 header에 menu_open추가
+        메뉴닫기를 클릭하면 header에 menu_close 클래스가 삭제
+        > 스크롤 금지
+        $("html, body").css({overflow : "hidden", height : $(window).height()
+        }).bind("scroll touchmove mousewheel", function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
+
+    */
+
+    $('header .gnb .gnb_open').on('click',function(){
+        $('.header').addClass('menu_open')
+        /* 스크롤 금지 */ 
+        $("html, body").css({overflow : "hidden", height : $(window).height()}).bind("scroll touchmove mousewheel", function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
+    })  
+    $('header .gnb .gnb_close').on('click',function(){
+        $("html, body").css({overflow : "hidden", height : $(window).height()}).bind("scroll touchmove mousewheel", function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
+        $('.header').removeClass('menu_open')
+    })  
+    
+
+    /*
+        1차메뉴를 a를 클릭하면
+            -a에 href링크를 작동안함...
+            - depth2는 slidedown으로 나타나게
+            -클릭한 a의 부모 li에 sub_open 클래스를 추가
+            만약에 이미 열려있는 메뉴라면 닫음
+            (열린메뉴를 판단하는 기준 - li에 sub_open이 있느냐 없느냐 )
+            --같은버튼이 열기/닫기를 동시에 하는 경우
+    */
+    $(".header .gnb ul.depth1 > li:has(.depth2) > a").on("click", function(e){
+        e.preventDefault();		/* a 태그의 href를 작동 시키지 않음 */
+        if($(this).parent().hasClass('sub_open') == true){ //sub_open 있다면
+            console.log('sub_open있음')
+            $(this).parent().removeClass('sub_open')
+            $(this).parent().find('ul.depth2').slideUp()
+        }else{
+            console.log('sub_open없음')
+            $(this).parent().addClass('sub_open')
+            $(this).parent().find('ul.depth2').slideDown()
+        }
+    });
 
 })
