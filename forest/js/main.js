99b3964995cacfbd5f131908b20ce4e36e0a3091
jooni02 <157003315+jooni02@users.noreply.github.com>
2024-03-21 14:23:56 +0900

    .

--- forest/js/main.js
+++ forest/js/main.js
@@ -13,12 +13,13 @@ $(document).ready(function(){
 		verticalCentered: true, /* 컨텐츠 요소 위아래 가운데 */
 
 		afterLoad: function(origin, destination, direction, trigger){
-			if((destination.index == 0)|| (destination.index == 2)){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-				console.log('검은색으로 바뀌어라');
+			if((destination.index == 0) || (destination.index == 2)){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				console.log('흰색으로 바뀌어라');
                 $('#fp-nav').attr('data-color', '')
                 $('header').removeClass('black')
 			}else{
-                console.log('흰색으로 바뀌어라');
+                console.log('검은색으로 바뀌어라');
+                $('#fp-nav').attr('data-color', 'black')
                 $('header').addClass('black')
             }
 		},
@@ -28,23 +29,30 @@ $(document).ready(function(){
 
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
+        effect: "fade", /* fade 효과 */
+
         autoplay: {  /* 팝업 자동 실행 */
-            delay: 2500,
+            delay: 5000,
             disableOnInteraction: true,
         },
 
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            el: '.visual .paging', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
             renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-                return '<span class="' + className + '">' + (index + 1) + "</span>";
+                return '<span class="page' + (index + 1) + ' '+className+'"><span></span></span>';
             },
+        },
+
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.swiper-button-next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.swiper-button-prev',  
         },
 
     });
     swiper.autoplay.stop();  /* 일시정지 기능 */
     swiper.autoplay.start();  /* 재생 기능 */
+
 })
