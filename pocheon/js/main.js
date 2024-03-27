b767c72f431306b742237f2f88d49a017129d741
jooni02 <157003315+jooni02@users.noreply.github.com>
2024-03-27 18:12:24 +0900

    .

--- pocheon/js/main.js
+++ pocheon/js/main.js
@@ -1,74 +1,57 @@
 $(document).ready(function(){
+    const swiper = new Swiper('.swiper', { /* 팝업을 감싼는 요소의 class명 */
 
-    const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
+    effect: "fade", /* 부드럽게 */ 
 
-		navigation: true, /* 오른쪽에 각 페이지의 paging */
-		navigationPosition: 'left', /* 위치 */
-		navigationTooltips: ['MAIN', '나무심기', '활동소개', '활동이야기', 'Footer'], /* 툴팁 */
-		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
+	autoplay: {  /* 팝업 자동 실행 */
+		delay: 5000,
+		disableOnInteraction: true,
+	},
 
-		autoScrolling:true, /* 한페이지씩 스크롤 */
-		scrollHorizontally: true,
+	loop: true,
 
-		verticalCentered: true, /* 컨텐츠 요소 위아래 가운데 */
-
-        scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
-
-		afterLoad: function(origin, destination, direction, trigger){
-			if((destination.index == 0) || (destination.index == 2)){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-				//console.log('흰색으로 바뀌어라');
-                $('#fp-nav').attr('data-color', '')
-                $('header').removeClass('black')
-			}else{
-                $('.counter').counterUp();
-                //console.log('검은색으로 바뀌어라');
-                $('#fp-nav').attr('data-color', 'black')
-                $('header').addClass('black')
-            }
+	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+		el: '.swiper-pagination', /* 해당 요소의 class명 */
+		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+		    return '<span class="' + className + '">' + (index + 1) + "</span>";
 		},
-
-		responsiveWidth: 768 /* fullpage를 적용시키지 않을 모바일 사이즈 */
+	},
 	});
 
-    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
-        effect: "fade", /* fade 효과 */
+	/* pc버전시 메뉴에 오버하면 hedaer 스타일 변경 */
+	let window_w // 브라우저 넓이
+	let device_status // 모바일모드인지 pc모드인지 구분
+	let mobile_size = 768
+	function device_chk(){
+		window_w = $(window).width()
+		if(window_w > mobile_size){ 
+			device_status = 'pc'
+		}else{ 
+			device_status = 'mobile'
+		}
+	}
+	device_chk()
+	$(window).resize(function(){
+		device_chk()
+	})
+	$('header .header_sub ul.depth1 > li').on('mouseenter focusin', function(){ /* 일회용 function*/
+		if(device_status == 'pc'){
+			console.log('dsfdsfsf');
+			$('header .header_sub .gnb ul.depth1 > li').removeClass('on') /* 메뉴오버하지 않을때 날릴태그*/
+			$(this).addClass('on')
+		}
+	})
+	$('header .header_sub .gnb ul.depth1 > li').on('mouseleave', function(){ /* 일회용 function*/
+		$('header .header_sub .gnb ul.depth1 > li').removeClass('on')
+		
+	})
 
-        autoplay: {  /* 팝업 자동 실행 */
-            delay: 5000,
-            disableOnInteraction: true,
-        },
-
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
-        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.visual .paging', /* 해당 요소의 class명 */
-            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-            renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-                return '<span class="page' + (index + 1) + ' '+className+'"><span></span></span>';
-            },
-        },
-
-        navigation: {  /* 이전, 다음 버튼 */
-            nextEl: '.swiper-button-next',  /* 다음 버튼의 클래스명 */
-            prevEl: '.swiper-button-prev',  
-        },
-
-    });//'.visual .swiper'
-
-    const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 'auto', /* 넓이고정 */
-        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-        breakpoints: {
-            768: {    /* 768px 이상일때 적용 */
-                slidesPerView: 3,
-                spaceBetween: 16,
-            },
-            1024: {   /* 1024px 이상일때 적용 */
-                slidesPerView: 4,
-                spaceBetween: 28,
-            },
-        },
-    });
+	//메뉴 바로 뒤에는 로그인 버튼이 있음, 로그인 버튼에 포커스 되면 메뉴의 오버상태 해제
+	$('.header .tnb03 .login').on('focusin',function(){
+		$('.header').removeClass('on')
+	})
 
 }) ////document.ready
