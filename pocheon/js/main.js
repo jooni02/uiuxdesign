426e7edc76ed66edf9ac6a96511ce2bde658cb31
jooni02 <157003315+jooni02@users.noreply.github.com>
2024-03-27 18:59:23 +0900

    .

---
+++ pocheon/js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    const swiper = new Swiper('.swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+    effect: "fade", /* 부드럽게 */ 
+
+	autoplay: {  /* 팝업 자동 실행 */
+		delay: 5000,
+		disableOnInteraction: true,
+	},
+
+	loop: true,
+
+	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+		el: '.swiper-pagination', /* 해당 요소의 class명 */
+		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+		    return '<span class="' + className + '">' + (index + 1) + "</span>";
+		},
+	},
+	});
+
+
+	/* pc버전시 메뉴에 오버하면 hedaer 스타일 변경 */
+	let window_w // 브라우저 넓이
+	let device_status // 모바일모드인지 pc모드인지 구분
+	let mobile_size = 768
+	function device_chk(){
+		window_w = $(window).width()
+		if(window_w > mobile_size){ 
+			device_status = 'pc'
+		}else{ 
+			device_status = 'mobile'
+		}
+	}
+	device_chk()
+	$(window).resize(function(){
+		device_chk()
+	})
+	$('header .header_sub ul.depth1 > li').on('mouseenter focusin', function(){ /* 일회용 function*/
+		if(device_status == 'pc'){
+			console.log('dsfdsfsf');
+			$('header .header_sub .gnb ul.depth1 > li').removeClass('on') /* 메뉴오버하지 않을때 날릴태그*/
+			$(this).addClass('on')
+		}
+	})
+	$('header .header_sub .gnb ul.depth1 > li').on('mouseleave', function(){ /* 일회용 function*/
+		$('header .header_sub .gnb ul.depth1 > li').removeClass('on')
+		
+	})
+
+	//메뉴 바로 뒤에는 로그인 버튼이 있음, 로그인 버튼에 포커스 되면 메뉴의 오버상태 해제
+	$('.header .tnb03 .login').on('focusin',function(){
+		$('.header').removeClass('on')
+	})
+
+}) ////document.ready
