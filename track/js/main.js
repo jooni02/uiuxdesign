c15583d47699ecfacc8d7d244974698461ee5246
jooni02 <157003315+jooni02@users.noreply.github.com>
2024-04-04 18:13:32 +0900

    20240404

--- track/js/main.js
+++ track/js/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
+    var sticky = new Sticky('[data-sticky]');   
     
+    var max = 200 //100% 투명할때의 스크롤 값
+    var min = 450
+    $(window).scroll(function(){
+        var scrollPX = $(this).scrollTop();
+        if( scrollPX  < max ) {
+            $(".intro .intro_tit").css({"opacity": (max-scrollPX)/max });
+        }else{
+            $(".intro .intro_tit").css({"opacity": 0});
+        }
+        if(scrollPX > min) {
+            $(".intro .bg").css({"opacity": (scrollPX-min)/min });
+        }else {
+            $(".intro .bg").css({"opacity": 0});
+        }
+
+    });
+
 }) ////document.ready
